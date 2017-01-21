4ffbb17261a10f17c6ad4c5bfbd5b92ba6c36e18
Pawel <zacdbg@gmail.com>
2017-01-21 20:22:25 +0100

    v7.1.0 - Initial legion fire support

--- main.lua
+++ main.lua
@@ -1,298 +1,158 @@
--- Author      : Kaminari
--- Create Date : 13:03 2015-04-20
-
--- Spells
--- Talents
-local _PrismaticCrystal = 152087;
-local _NetherTempest = 114923;
-local _RuneOfPower = 116011;
-local _Supernova = 157980;
-local _ArcaneOrb = 153626;
-local _LivingBomb = 44457;
-local _BlastWave = 157981;
--- Arcane
-local _ArcaneBlast = 30451;
-local _ArcaneBarrage = 44425;
-local _ArcaneMissiles = 5143;
-local _ArcanePower = 12042;
-local _Evocation = 12051;
-local _PresenceOfMind = 12043;
 -- Fire
-local _Combustion = 11129;
+local _RuneofPower = 116011;
+local _Combustion = 190319;
+local _PhoenixsFlames = 194466;
+local _FlameOn = 205029;
+local _Flamestrike = 2120;
+local _HotStreak = 195283;
+local _HotStreakAura = 48108;
 local _Pyroblast = 11366;
-local _InfernoBlast = 108853;
+local _Meteor = 153561;
+local _LivingBomb = 44457;
+local _FireBlast = 108853;
+local _DragonsBreath = 31661;
+local _HeatingUp = 48107;
 local _Fireball = 133;
-local _DragonBreath = 31661;
--- Frost
-local _FrozenOrb = 84714;
-local _Frostbolt = 116;
-local _IceLance = 30455;
-local _FrostfireBolt = 44614;
-local _IceNova = 157997;
-local _SummonWaterElemental = 157750;
-local _Freeze = 33395;
-local _WaterJet = 135029;
-
-
--- Auras
-local _RuneOfPowerAura = 116014;
-local _ArcaneMissilesAura = 79683;
-local _HeatingAura = 48107;
-local _PyroblastAura = 48108;
-local _FingersOfFrost = 44544;
-local _BrainFreeze = 57761;
-local _MarkofDoom = 184073;
+local _Scorch = 2948;
+local _IceFloes = 108839;
+local _Ignite = 12654;
+local _MirrorImage = 55342;
+local _BlastWave = 157981;
+local _Cinderstorm = 198929;
+local _FrostNova = 122;
+local _Blink = 1953;
+local _IceBlock = 45438;
+local _Kindling = 155148;
+local _IceBarrier = 11426;
+local _Shimmer = 212653;
+local _Pyromaniac = 205020;
 
 -- Talents
-local _isRuneOfPower = false;
-local _isMirrorImage = false;
-local _isPrismaticCrystal = false;
-local _isArcaneOrb = false;
-local _isNetherTempest = false;
-local _isSupernova = false;
-local _isLivingBomb = false;
+local _isFlameOn = false;
 local _isBlastWave = false;
-local _isIceNova = false;
+local _isMeteor = false;
+local _isCinderstorm = false;
+local _isMirrorImage = false;
+local _isRuneofPower = false;
 
-local _Burn = false;
-local _evoFlag = false;
+-- Legendary items
+local _isKoralon = false;
+local _isDarckli = false;
 
-_CombustionValue = 8000;
 _BaseArcaneBlastCost = 3200;
 
-----------------------------------------------
--- Pre enable, checking talents
-----------------------------------------------
-TDDps_Mage_CheckTalents = function()
-	-- Tier 6 talents
-	_isRuneOfPower = TD_TalentEnabled('Rune of Power');
-	_isMirrorImage = TD_TalentEnabled('Mirror Image');
-
-	-- Tier 7 talents
-	_isPrismaticCrystal = TD_TalentEnabled('Prismatic Crystal');
-	_isArcaneOrb = TD_TalentEnabled('Arcane Orb');
-
-	_isNetherTempest = TD_TalentEnabled('Nether Tempest');
-	_isSupernova = TD_TalentEnabled('Supernova');
-	_isLivingBomb = TD_TalentEnabled('Living Bomb');
-	_isBlastWave = TD_TalentEnabled('Blast Wave');
-	_isIceNova = TD_TalentEnabled('Ice Nova');
-
-	_BaseArcaneBlastCost = 3200;
-	-- other checking functions
+MaxDps.Mage = {};
+function MaxDps.Mage.CheckTalents()
+	MaxDps:CheckTalents();
+	_isFlameOn = MaxDps:HasTalent(_FlameOn);
+	_isBlastWave = MaxDps:HasTalent(_BlastWave);
+	_isMeteor = MaxDps:HasTalent(_Meteor);
+	_isCinderstorm = MaxDps:HasTalent(_Cinderstorm);
+	_isMirrorImage = MaxDps:HasTalent(_MirrorImage);
+	_isRuneofPower = MaxDps:HasTalent(_RuneofPower);
+	_isKoralon = IsEquippedItem(132454);
+	_isDarckli = IsEquippedItem(132863);
 end
 
-----------------------------------------------
--- Enabling Addon
-----------------------------------------------
-function TDDps_Mage_EnableAddon(mode)
+function MaxDps:EnableRotationModule(mode)
 	mode = mode or 1;
-	_TD['DPS_Description'] = 'TD Mage DPS supports: Arcane';
-	_TD['DPS_OnEnable'] = TDDps_Mage_CheckTalents;
+	MaxDps.Description = 'Mage [Fire]';
+	MaxDps.ModuleOnEnable = MaxDps.Mage.CheckTalents;
 	if mode == 1 then
-		_TD['DPS_NextSpell'] = TDDps_Mage_Arcane;
+		MaxDps.NextSpell = MaxDps.Mage.Arcane;
 	end;
 	if mode == 2 then
-		_TD['DPS_NextSpell'] = TDDps_Mage_Fire;
+		MaxDps.NextSpell = MaxDps.Mage.Fire;
 	end;
 	if mode == 3 then
-		_TD['DPS_NextSpell'] = TDDps_Mage_Frost;
+		MaxDps.NextSpell = MaxDps.Mage.Frost;
 	end;
-	TDDps_EnableAddon();
 end
 
-----------------------------------------------
--- Main rotation: Arcane
-----------------------------------------------
-TDDps_Mage_Arcane = function()
-	local timeShift, currentSpell = TD_EndCast();
+function MaxDps.Mage.Arcane()
+	local timeShift, currentSpell = MaxDps:EndCast();
 
-	local arcaneStacks = TDDps_Mage_ArcaneCharge();
-	local mana = TD_Mana(0, timeShift);
-	local evo = TD_SpellAvailable(_Evocation, timeShift);
-	local ap, apCd = TD_SpellAvailable(_ArcanePower, timeShift);
-	local ao = TD_SpellAvailable(_ArcaneOrb, timeShift);
-	local pom = TD_SpellAvailable(_PresenceOfMind, timeShift);
-	local prism = TD_SpellAvailable(_PrismaticCrystal, timeShift);
-	local nt = TD_TargetAura(_NetherTempest, timeShift + 3.6);
-	local mod = TD_TargetAura(_MarkofDoom, timeShift);
-	local sn, snCharges, snMax = TD_SpellCharges(_Supernova);
-	local am, amCharges = TD_Aura(_ArcaneMissilesAura, timeShift);
-	local apAura = TD_Aura(_ArcanePower, timeShift);
-	local rop = TDDps_Mage_RuneOfPower();
-
-	if amCharges > 0 and currentSpell == 'Arcane Charges' then
-		amCharges = amCharges - 1;
-	end
-
-	local abMulti = arcaneStacks;
-	if currentSpell == 'Arcane Blast' then
-		local abCost = _BaseArcaneBlastCost * abMulti;
-		if apAura then
-			abCost = abCost * 0.9;
-		end
-		abCost = abCost / UnitPowerMax('player', 0);
-
-		mana = mana - abCost;
-	end
-
-	TDButton_GlowCooldown(_ArcanePower, ap);
-	TDButton_GlowCooldown(_PresenceOfMind, pom);
-	TDButton_GlowCooldown(_PrismaticCrystal, prism);
-
-	if apCd >= 89 and not _Burn then
-		print('Burn faza');
-		_Burn = true;
-	end
-
-	if _isRuneOfPower and not rop and currentSpell ~= 'Rune of Power' then
-		return _RuneOfPower;
-	end
-
-	-- Conserve rotation
-	if (_Burn and mana < 0.5) or (_evoFlag and mana < 0.9) then
-		_evoFlag = true;
-		return _Evocation;
-	end
-
-	if _evoFlag and mana > 0.9 then
-		_evoFlag = false;
-		_Burn = false;
-	end
-
-	if mod and (amCharges > 1 or (amCharges > 0 and currentSpell ~= 'Arcane Missiles')) then
-		return _ArcaneMissiles;
-	end
-
-	if _isNetherTempest and arcaneStacks == 4 and not nt then
-		return _NetherTempest;
-	end
-
-	if _isSupernova and snCharges == snMax then
-		return _Supernova;
-	end
-
-	if _isArcaneOrb and ao and arcaneStacks < 2 then
-		return _ArcaneOrb;
-	end
-
-	if _Burn and amCharges >= 1 and mana > 0.7 and currentSpell ~= 'Arcane Missiles' then
-		return _ArcaneMissiles;
-	end
-
-	if mana > 0.93 and currentSpell ~= 'Arcane Blast' then
-		return _ArcaneBlast;
-	end
-
-	if arcaneStacks == 4 and (amCharges > 1 or (amCharges > 0 and currentSpell ~= 'Arcane Missiles')) then
-		return _ArcaneMissiles;
-	end
-
-	if not _Burn and (arcaneStacks == 4 or (mana < 0.9 and arcaneStacks >= 1)) then
-		return _ArcaneBarrage;
-	end
-
-	return _ArcaneBlast;
+	return nil;
 end
 
-----------------------------------------------
--- Main rotation: Fire
-----------------------------------------------
-TDDps_Mage_Fire = function()
-	local timeShift, currentSpell = TD_EndCast();
+function MaxDps.Mage.Fire()
+	local timeShift, currentSpell = MaxDps:EndCast();
 
-	local rop = TDDps_Mage_RuneOfPower();
-	local ignite = TDDps_Mage_Ignite();
-	local pyro = TD_Aura(_PyroblastAura, timeShift);
-	local heating = TD_Aura(_HeatingAura, timeShift);
-	local combustion = TD_SpellAvailable(_Combustion, timeShift);
-	local inferno = TD_SpellAvailable(_InfernoBlast, timeShift);
-	local blast = TD_SpellAvailable(_BlastWave, timeShift);
-	local dragon = TD_SpellAvailable(_DragonBreath, timeShift);
-	local livingBomb = TD_TargetAura(_LivingBomb, timeShift + 3.6);
+	MaxDps:GlowCooldown(_Combustion, MaxDps:SpellAvailable(_Combustion, timeShift));
+	MaxDps:GlowCooldown(_RuneofPower, _isRuneofPower and MaxDps:SpellAvailable(_RuneofPower, timeShift));
+	MaxDps:GlowCooldown(_MirrorImage, _isMirrorImage and MaxDps:SpellAvailable(_MirrorImage, timeShift));
 
-	TDButton_GlowCooldown(_DragonBreath, dragon);
+	local combu, combuCD = MaxDps:Aura(_Combustion, timeShift);
+	local rop = MaxDps:PersistentAura(_RuneofPower);
 
-	if _isRuneOfPower and not rop and currentSpell ~= 'Rune of Power' then
-		return _RuneOfPower;
+	local pf, pfCharges = MaxDps:SpellCharges(_PhoenixsFlames, timeShift);
+	local fb, fbCharges = MaxDps:SpellCharges(_FireBlast, timeShift);
+
+	local ph = MaxDps:TargetPercentHealth();
+
+	if pfCharges >= 2 then
+		return _PhoenixsFlames;
 	end
 
-	if ignite and ignite >= _CombustionValue and combustion then
-		return _Combustion;
-	end
-
-	if pyro and heating then
+	if MaxDps:Aura(_HotStreakAura, timeShift) then
 		return _Pyroblast;
 	end
 
-	if _isLivingBomb and not livingBomb then
-		return _LivingBomb;
+	if _isFlameOn and fbCharges == 0 and MaxDps:SpellAvailable(_FlameOn, timeShift) then
+		return _FlameOn;
 	end
 
-	if heating and inferno then
-		return _InfernoBlast;
-	end
-
-	if _isBlastWave and blast then
+	--actions.active_talents+=/blast_wave,if=(buff.combustion.down)|(buff.combustion.up&action.fire_blast
+	--.charges<1&action.phoenixs_flames.charges<1)
+	if _isBlastWave and MaxDps:SpellAvailable(_BlastWave, timeShift) and
+		((not combu) or
+		(combu and fbCharges < 1 and pfCharges < 1))
+	then
 		return _BlastWave;
+	end
+
+	--actions.active_talents+=/meteor,if=cooldown.combustion.remains>30|(cooldown.combustion.remains>target
+	--.time_to_die)|buff.rune_of_power.up
+	if _isMeteor and MaxDps:SpellAvailable(_Meteor, timeShift) and ((combuCD > 30) or rop) then
+		return _Meteor
+	end
+
+	--actions.active_talents+=/cinderstorm,if=cooldown.combustion.remains<cast_time&(buff.rune_of_power.up|!talent
+	--.rune_on_power.enabled)|cooldown.combustion.remains>10*spell_haste&!buff.combustion.up
+	if _isCinderstorm and MaxDps:SpellAvailable(_Cinderstorm, timeShift) and
+		not MaxDps:SameSpell(currentSpell, _Cinderstorm) and
+		not combu and not rop then
+		return _Cinderstorm;
+	end
+
+	--actions.active_talents+=/dragons_breath,if=equipped.132863
+	if _isDarckli and MaxDps:SpellAvailable(_DragonsBreath, timeShift) then
+		return _DragonsBreath;
+	end
+
+	--actions.active_talents+=/living_bomb,if=active_enemies>1&buff.combustion.down
+	--NIY
+
+	if fbCharges > 0 and MaxDps:Aura(_HeatingUp, timeShift) then
+		return _FireBlast;
+	end
+
+	local moving = GetUnitSpeed('player');
+	if (_isKoralon and ph < 0.25) or moving > 0 then
+		return _Scorch;
 	end
 
 	return _Fireball;
 end
 
-----------------------------------------------
--- Main rotation: Frost
-----------------------------------------------
-TDDps_Mage_Frost = function()
-	local timeShift, currentSpell = TD_EndCast();
-	local _, currentPetSpell = TD_EndCast('pet');
+function MaxDps.Mage.Frost()
+	local timeShift, currentSpell = MaxDps:EndCast();
+	local _, currentPetSpell = MaxDps:EndCast('pet');
 
-	local orb, orbCD = TD_SpellAvailable(_FrozenOrb, timeShift);
-	local orbActive = orbCD > 50;
-
-	local elementalCD = TD_SpellAvailable(_SummonWaterElemental, timeShift);
-	local freeze = TD_SpellAvailable(_Freeze, timeShift);
-	local jet = TD_SpellAvailable(_WaterJet, timeShift);
-	local _, novaCharges = TD_SpellCharges(_IceNova, timeShift);
-	local targetPH = TD_TargetPercentHealth();
-	local elemental = UnitExists('pet');
-	local fingers = TD_Aura(_FingersOfFrost, timeShift);
-	local brain = TD_Aura(_BrainFreeze, timeShift);
-
-	TDButton_GlowCooldown(_FrozenOrb, orb);
-	TDButton_GlowCooldown(_Freeze, elemental and freeze and not orbActive);
-
-	if not elemental and elementalCD and currentSpell ~= 'Summon Water Elemental' then
-		return _SummonWaterElemental;
-	end
-
-	if _isIceNova and (novaCharges > 1 or (targetPH < 0.1 and novaCharges >= 1)) then
-		return _IceNova;
-	end
-
-	if fingers then
-		return _IceLance;
-	end
-
-	if brain then
-		return _FrostfireBolt;
-	end
-
-	if not orbActive and jet and not fingers and currentPetSpell ~= 'Water Jet' then
-		return _WaterJet;
-	end
-
-	if _isIceNova and novaCharges >= 1 then
-		return _IceNova;
-	end
-
-	return _Frostbolt;
+	return nil;
 end
 
-----------------------------------------------
--- Arcane Charges
-----------------------------------------------
-function TDDps_Mage_ArcaneCharge()
+function MaxDps.Mage.ArcaneCharge()
 	local _, _, _, charges = UnitAura('player', 'Arcane Charge', nil, 'PLAYER|HARMFUL');
 	if charges == nil then
 		charges = 0;
@@ -300,11 +160,11 @@ function TDDps_Mage_ArcaneCharge()
 	return charges;
 end
 
-function TDDps_Mage_RuneOfPower()
+function MaxDps.Mage.RuneOfPower()
 	local n = UnitAura('player', 'Rune of Power')
 	return n == 'Rune of Power';
 end
 
-function TDDps_Mage_Ignite()
+function MaxDps.Mage.Ignite()
 	return select(15, UnitAura('target', 'Ignite', nil, 'HARMFUL'));
 end
