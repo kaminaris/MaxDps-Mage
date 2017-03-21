7f003dc24bfce4be077b176318a4989c1cc81dd3
Pawel <zacdbg@gmail.com>
2017-03-21 02:43:47 +0100

    v7.1.5 - Frost support

--- main.lua
+++ main.lua
@@ -1,5 +1,4 @@
 -- Fire
-local _RuneofPower = 116011;
 local _Combustion = 190319;
 local _PhoenixsFlames = 194466;
 local _FlameOn = 205029;
@@ -16,7 +15,6 @@ local _Fireball = 133;
 local _Scorch = 2948;
 local _IceFloes = 108839;
 local _Ignite = 12654;
-local _MirrorImage = 55342;
 local _BlastWave = 157981;
 local _Cinderstorm = 198929;
 local _FrostNova = 122;
@@ -26,6 +24,42 @@ local _Kindling = 155148;
 local _IceBarrier = 11426;
 local _Shimmer = 212653;
 local _Pyromaniac = 205020;
+
+-- Frost
+local _IcyVeins = 12472;
+local _RayofFrost = 205021;
+local _IceLance = 30455;
+local _FingersofFrost = 112965;
+local _FrostBomb = 112948;
+local _FrozenOrb = 84714;
+local _Freeze = 33395;
+local _Ebonbolt = 214634;
+local _BrainFreeze = 190447;
+local _Flurry = 44614;
+local _Frostbolt = 116;
+local _WaterJet = 135029;
+local _IceNova = 157997;
+local _CometStorm = 153595;
+local _Blizzard = 190356;
+local _ArcticGale = 205038;
+local _GlacialSpike = 199786;
+local _IncantersFlow = 1463;
+local _ThermalVoid = 155149;
+local _SplittingIce = 56377;
+local _IceFloes = 108839;
+local _Blink = 1953;
+local _IceBarrier = 11426;
+local _Shimmer = 212653;
+local _SummonWaterElemental = 31687;
+local _Icicles = 205473;
+local _LonelyWinter = 205024;
+local _ChainReaction = 195419;
+local _WintersChill = 228358;
+
+-- CDs
+
+local _MirrorImage = 55342;
+local _RuneofPower = 116011;
 
 -- Talents
 local _isFlameOn = false;
@@ -40,10 +74,12 @@ local _isKoralon = false;
 local _isDarckli = false;
 
 _BaseArcaneBlastCost = 3200;
+local talents = {};
 
 MaxDps.Mage = {};
 function MaxDps.Mage.CheckTalents()
 	MaxDps:CheckTalents();
+	talents = MaxDps.PlayerTalents;
 	_isFlameOn = MaxDps:HasTalent(_FlameOn);
 	_isBlastWave = MaxDps:HasTalent(_BlastWave);
 	_isMeteor = MaxDps:HasTalent(_Meteor);
@@ -149,7 +185,64 @@ function MaxDps.Mage.Frost()
 	local timeShift, currentSpell = MaxDps:EndCast();
 	local _, currentPetSpell = MaxDps:EndCast('pet');
 
-	return nil;
+	local rop = MaxDps:PersistentAura(_RuneofPower);
+	local fof, fofCharges = MaxDps:Aura(_FingersofFrost, timeShift);
+	local ici, iciCharges = MaxDps:Aura(_Icicles, timeShift);
+	local bf, bfCd = MaxDps:Aura(_BrainFreeze, timeShift);
+	local cr, crCharges, crCd = MaxDps:Aura(_ChainReaction, timeShift);
+
+	local elemental = UnitExists('pet');
+
+	MaxDps:GlowCooldown(_RuneofPower, talents[_RuneofPower] and MaxDps:SpellAvailable(_RuneofPower, timeShift));
+	MaxDps:GlowCooldown(_MirrorImage, talents[_MirrorImage] and MaxDps:SpellAvailable(_MirrorImage, timeShift));
+
+	if not talents[_LonelyWinter] and not elemental and MaxDps:SpellAvailable(_SummonWaterElemental, timeShift) and
+			not MaxDps:SameSpell(currentSpell, _SummonWaterElemental) then
+		return _SummonWaterElemental;
+	end
+
+	if talents[_RayofFrost] and MaxDps:SpellAvailable(_RayofFrost, timeShift) then
+		if talents[_RuneofPower] then
+			if rop then
+				return _RayofFrost;
+			end
+		else
+			return _RayofFrost;
+		end
+	end
+
+	if fofCharges >= 3 then
+		return _IceLance;
+	end
+
+	if talents[_FrostBomb] and fofCharges >= 1 and not MaxDps:SameSpell(currentSpell, _FrostBomb) and not
+		MaxDps:TargetAura(_FrostBomb, timeShift) then
+		return _FrostBomb;
+	end
+
+	if bf then
+		return _Flurry;
+	end
+
+	if MaxDps:SameSpell(currentSpell, _Flurry) or MaxDps:TargetAura(_WintersChill, timeShift) then
+		return _IceLance;
+	end
+
+	if not talents[_LonelyWinter] and MaxDps:SpellAvailable(_WaterJet, timeShift) and not fof and
+			not MaxDps:SameSpell(currentPetSpell, _WaterJet) then
+		return _WaterJet;
+	end
+
+	--Chain reaction
+	if fofCharges >= 1 and (crCd < 1 or crCharges >= 3) then
+		return _IceLance;
+	end
+
+	if talents[_GlacialSpike] and iciCharges >= 5 and not MaxDps:SameSpell(currentSpell, _GlacialSpike) then
+		return _GlacialSpike;
+	end
+
+	return _Frostbolt;
 end
 
 function MaxDps.Mage.ArcaneCharge()
