abd5e8f55d9ccccc719057c33d0640106c586874
tbkizle <tbkizle@gmail.com>
2024-06-09 23:47:26 -0400

    format and print update

--- Main.lua
+++ Main.lua
@@ -1,30 +1,30 @@
-local addonName, addonTable = ...;
-_G[addonName] = addonTable;
+local addonName, addonTable = ...
+_G[addonName] = addonTable
 
 --- @type MaxDps
 if not MaxDps then return end
 
-local MaxDps = MaxDps;
-local Mage = MaxDps:NewModule('Mage');
-addonTable.Mage = Mage;
+local MaxDps = MaxDps
+local Mage = MaxDps:NewModule('Mage')
+addonTable.Mage = Mage
 
 Mage.spellMeta = {
 	__index = function(t, k)
-		print('Spell Key ' .. k .. ' not found!');
+		print('Spell Key ' .. k .. ' not found!')
 	end
 }
 
 function Mage:Enable()
 	if MaxDps.Spec == 1 then
-		MaxDps:Print(MaxDps.Colors.Info .. 'Mage Arcane');
-		MaxDps.NextSpell = Mage.Arcane;
+		MaxDps:Print(MaxDps.Colors.Info .. 'Mage Arcane', "info")
+		MaxDps.NextSpell = Mage.Arcane
 	elseif MaxDps.Spec == 2 then
-		MaxDps:Print(MaxDps.Colors.Info .. 'Mage Fire');
-		MaxDps.NextSpell = Mage.Fire;
+		MaxDps:Print(MaxDps.Colors.Info .. 'Mage Fire', "info")
+		MaxDps.NextSpell = Mage.Fire
 	elseif MaxDps.Spec == 3 then
-		MaxDps:Print(MaxDps.Colors.Info .. 'Mage Frost');
-		MaxDps.NextSpell = Mage.Frost;
+		MaxDps:Print(MaxDps.Colors.Info .. 'Mage Frost', "info")
+		MaxDps.NextSpell = Mage.Frost
 	end
 
-	return true;
+	return true
 end
